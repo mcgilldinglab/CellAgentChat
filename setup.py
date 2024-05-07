a3d5a996de00985a9eae622a879dfe517cea0aed
vishvak11 <77021753+vishvak11@users.noreply.github.com>
2024-05-07 13:40:12 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CellAgentChat",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=["numpy >= 1.22.3",
+    'pandas >= 1.5.0',
+    'scanpy >= 1.9.1'
+    'Mesa >= 1.0.0',
+    'torch >= 1.12.1',
+    'scipy >= 1.9.1',
+    'seaborn >= 0.12.0',
+    'matplotlib >= 3.6.0',
+    'pyslingshot >= 0.0.2',
+    'anndata >= 0.8.0',
+    'scanpy >= 1.9.6'
+    ],
+    author='Vishvak Raghavan',
+    author_email='vishvak.raghavan@mail.mcgill.ca',
+    description='Harnessing Agent-Based Modeling in CellAgentChat to Unravel Cell-Cell Interactions from Single-Cell Data',
+    url='https://github.com/mcgilldinglab/CellAgentChat',
+)
